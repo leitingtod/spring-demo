cd4fd41574f9029d4b48d9b8d1669fcb8f65edcb
liuyazhou <leitingtod@126.com>
2018-10-08 17:14:36 +0800

    spring guide - Serving Web Content with Spring MVC works

--- src/main/java/com/example/demo/form/GreetingController.java
+++ src/main/java/com/example/demo/form/GreetingController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
@@ -18,6 +19,12 @@ public class GreetingController {
     @PostMapping("/greeting")
     public String greetingSubmit(@ModelAttribute Greeting greeting) {
         return "result";
+    }
+
+    @GetMapping("/welcome")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "welcome";
     }
 
 }
