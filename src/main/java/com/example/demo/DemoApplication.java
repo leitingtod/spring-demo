d2713e61e59ab30733a323eb6a092b6554d4d008
liuyazhou <leitingtod@126.com>
2018-10-22 10:57:18 +0800

    spring uploading&downloading file works

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.upload.StorageProperties;
+import com.example.demo.upload.StorageService;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.mysql")
+@EnableConfigurationProperties(StorageProperties.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+
 }
